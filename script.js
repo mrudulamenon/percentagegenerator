3495cf37856f76926abc5d86086224e11e376700
mrudulamenon <mrudulammenon@gmail.com>
2020-04-13 17:05:39 +0530

    Add style

--- script.js
+++ script.js
@@ -2,28 +2,35 @@
 function getPercentage()
 {
     alert("hi");
-    var card = document.getElementById("reportCard");
-    var studPara = document.getElementById("displayPara");
-    var head = document.getElementById("cardHead");
-    var studName = document.getElementById("name");
-    var studClass = document.getElementById("class");
-    var studRollNo = document.getElementById("rollNo");
+    //var card = document.getElementById("reportCard");
+    var studPara = document.querySelector("#displayPara");
+    var head = document.querySelector("#cardHead");
+    var studName = document.querySelector("#name");
+    var studClass = document.querySelector("#class");
+    var studRollNo = document.querySelector("#rollNo");
+    var avgSpan = document.querySelector("#avgDisplay")
 
     //card.removeAttribute("hidden","true");
     //card.setAttribute("hidden","false");
-    head.innerHTML="Percentage Card";
+    head.innerText="Percentage Card";
 
     var sub1 = parseFloat(document.getElementById("mark1").value);
-    alert(sub1);
+    //alert(sub1);
     var sub2 = parseFloat(document.getElementById("mark2").value);
     var sub3 = parseFloat(document.getElementById("mark3").value);
     var percentage = (sub1 + sub2 + sub3)/3;
-    alert (percentage);
-    para.innerHTML="The percentage obtained by your ward" + studName + " of Class " +studClass + " with RollNo" + studRollNo + "is"+  percentage;
+    alert ("Percentage is "+ percentage);
+
+    studPara.innerText="The percentage obtained by your ward" + studName + " of Class " +studClass + " with RollNo" + studRollNo + "has scored";
+    avgSpan.innerHTML="Percentage" + percentage;
+    if(percentage<40)
+    { avgSpan.style.color="red"; }
+    else if(percentage<70){
+        avgSpan.style.color="green";
+    }
+    else
+    { avgSpan.style.color="white";}
+
     //para.innerText="hi";
-
-
-
-
 
 }
